4f88ce472da854673714c8ba71355522ab412d8e
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-06-15 22:29:21 +0530

    Create malayalam.py

---
+++ malayalam.py
@@ -0,0 +1,22 @@
+# malayalam.py - Malayalam Songs Database (Local, no API call needed)
+MALAYALAM_SONGS = [
+    {"id": "1", "title": "Malare", "artist": "Vijay Yesudas", "image": "https://c.saavncdn.com/artists/Vijay_Yesudas_004_20230301111756_500x500.jpg", "language": "malayalam"},
+    {"id": "2", "title": "Darshana", "artist": "Hesham Abdul Wahab", "image": "https://c.saavncdn.com/artists/Hesham_Abdul_Wahab_001_20220721151610_500x500.jpg", "language": "malayalam"},
+    {"id": "3", "title": "Jimikki Kammal", "artist": "Shaan Rahman", "image": "https://c.saavncdn.com/artists/Shaan_Rahman_003_20230301114324_500x500.jpg", "language": "malayalam"},
+    {"id": "4", "title": "Kadhale Kadhale", "artist": "Gopi Sundar", "image": "https://c.saavncdn.com/artists/Gopi_Sundar_003_20230301111652_500x500.jpg", "language": "malayalam"},
+    {"id": "5", "title": "Hridayam", "artist": "Hesham Abdul Wahab", "image": "https://c.saavncdn.com/artists/Hesham_Abdul_Wahab_001_20220721151610_500x500.jpg", "language": "malayalam"},
+    {"id": "6", "title": "Pala Palli", "artist": "Jakes Bejoy", "image": "https://c.saavncdn.com/artists/Jakes_Bejoy_003_20230301121051_500x500.jpg", "language": "malayalam"},
+    {"id": "7", "title": "Kalapakkara", "artist": "Jakes Bejoy", "image": "https://c.saavncdn.com/artists/Jakes_Bejoy_003_20230301121051_500x500.jpg", "language": "malayalam"},
+    {"id": "8", "title": "Appangal Embadam", "artist": "Shaan Rahman", "image": "https://c.saavncdn.com/artists/Shaan_Rahman_003_20230301114324_500x500.jpg", "language": "malayalam"},
+    {"id": "9", "title": "Pavizha Mazha", "artist": "K. S. Harisankar", "image": "https://c.saavncdn.com/artists/K_S_Harisankar_003_20230301120815_500x500.jpg", "language": "malayalam"},
+    {"id": "10", "title": "Thallumaala", "artist": "Vishnu Vijay", "image": "https://c.saavncdn.com/artists/Vishnu_Vijay_000_20200213123849_500x500.jpg", "language": "malayalam"},
+]
+
+MALAYALAM_ARTISTS = [
+    {"id": "hesham_abdul_wahab", "name": "Hesham Abdul Wahab", "image": "https://c.saavncdn.com/artists/Hesham_Abdul_Wahab_001_20220721151610_500x500.jpg", "language": "malayalam"},
+    {"id": "gopi_sundar", "name": "Gopi Sundar", "image": "https://c.saavncdn.com/artists/Gopi_Sundar_003_20230301111652_500x500.jpg", "language": "malayalam"},
+    {"id": "shaan_rahman", "name": "Shaan Rahman", "image": "https://c.saavncdn.com/artists/Shaan_Rahman_003_20230301114324_500x500.jpg", "language": "malayalam"},
+    {"id": "jakes_bejoy", "name": "Jakes Bejoy", "image": "https://c.saavncdn.com/artists/Jakes_Bejoy_003_20230301121051_500x500.jpg", "language": "malayalam"},
+    {"id": "vijay_yesudas", "name": "Vijay Yesudas", "image": "https://c.saavncdn.com/artists/Vijay_Yesudas_004_20230301111756_500x500.jpg", "language": "malayalam"},
+]
+
