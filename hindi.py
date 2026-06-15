4c6fa0ea15caa7ce44e69d8c00173c2051c53cd9
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-06-15 21:45:02 +0530

    Add files via upload

---
+++ hindi.py
@@ -0,0 +1,21 @@
+# hindi.py - Hindi Songs Database (Local, no API call needed)
+HINDI_SONGS = [
+    {"id": "1", "title": "Tum Hi Ho", "artist": "Arijit Singh", "image": "https://c.saavncdn.com/artists/Arijit_Singh_002_20230301073043_500x500.jpg", "language": "hindi"},
+    {"id": "2", "title": "Channa Mereya", "artist": "Arijit Singh", "image": "https://c.saavncdn.com/artists/Arijit_Singh_002_20230301073043_500x500.jpg", "language": "hindi"},
+    {"id": "3", "title": "Raataan Lambiyan", "artist": "Jubin Nautiyal", "image": "https://c.saavncdn.com/artists/Jubin_Nautiyal_002_20230301092520_500x500.jpg", "language": "hindi"},
+    {"id": "4", "title": "Kesariya", "artist": "Arijit Singh", "image": "https://c.saavncdn.com/artists/Arijit_Singh_002_20230301073043_500x500.jpg", "language": "hindi"},
+    {"id": "5", "title": "Apna Bana Le", "artist": "Arijit Singh", "image": "https://c.saavncdn.com/artists/Arijit_Singh_002_20230301073043_500x500.jpg", "language": "hindi"},
+    {"id": "6", "title": "Phir Aur Kya Chahiye", "artist": "Arijit Singh", "image": "https://c.saavncdn.com/artists/Arijit_Singh_002_20230301073043_500x500.jpg", "language": "hindi"},
+    {"id": "7", "title": "O Bedardeya", "artist": "Arijit Singh", "image": "https://c.saavncdn.com/artists/Arijit_Singh_002_20230301073043_500x500.jpg", "language": "hindi"},
+    {"id": "8", "title": "Tere Pyaar Mein", "artist": "Arijit Singh", "image": "https://c.saavncdn.com/artists/Arijit_Singh_002_20230301073043_500x500.jpg", "language": "hindi"},
+    {"id": "9", "title": "Satranga", "artist": "Arijit Singh", "image": "https://c.saavncdn.com/artists/Arijit_Singh_002_20230301073043_500x500.jpg", "language": "hindi"},
+    {"id": "10", "title": "Lutt Putt Gaya", "artist": "Arijit Singh", "image": "https://c.saavncdn.com/artists/Arijit_Singh_002_20230301073043_500x500.jpg", "language": "hindi"},
+]
+
+HINDI_ARTISTS = [
+    {"id": "arijit_singh", "name": "Arijit Singh", "image": "https://c.saavncdn.com/artists/Arijit_Singh_002_20230301073043_500x500.jpg", "language": "hindi"},
+    {"id": "jubin_nautiyal", "name": "Jubin Nautiyal", "image": "https://c.saavncdn.com/artists/Jubin_Nautiyal_002_20230301092520_500x500.jpg", "language": "hindi"},
+    {"id": "neha_kakkar", "name": "Neha Kakkar", "image": "https://c.saavncdn.com/artists/Neha_Kakkar_006_20230302130220_500x500.jpg", "language": "hindi"},
+    {"id": "atif_aslam", "name": "Atif Aslam", "image": "https://c.saavncdn.com/artists/Atif_Aslam_003_20230301145647_500x500.jpg", "language": "hindi"},
+    {"id": "shreya_ghoshal", "name": "Shreya Ghoshal", "image": "https://c.saavncdn.com/artists/Shreya_Ghoshal_007_20230301102547_500x500.jpg", "language": "hindi"},
+]
