7549b37ac1601f6c53b57ed51f25b50bb7791af0
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-06-15 21:46:39 +0530

    Add files via upload

---
+++ punjabi.py
@@ -0,0 +1,21 @@
+# punjabi.py - Punjabi Songs Database (Local, no API call needed)
+PUNJABI_SONGS = [
+    {"id": "p1", "title": "Lover", "artist": "Diljit Dosanjh", "image": "https://c.saavncdn.com/artists/Diljit_Dosanjh_003_20230301145647_500x500.jpg", "language": "punjabi"},
+    {"id": "p2", "title": "Do You Know", "artist": "Diljit Dosanjh", "image": "https://c.saavncdn.com/artists/Diljit_Dosanjh_003_20230301145647_500x500.jpg", "language": "punjabi"},
+    {"id": "p3", "title": "GOAT", "artist": "Diljit Dosanjh", "image": "https://c.saavncdn.com/artists/Diljit_Dosanjh_003_20230301145647_500x500.jpg", "language": "punjabi"},
+    {"id": "p4", "title": "Chauffeur", "artist": "Diljit Dosanjh", "image": "https://c.saavncdn.com/artists/Diljit_Dosanjh_003_20230301145647_500x500.jpg", "language": "punjabi"},
+    {"id": "p5", "title": "Born To Shine", "artist": "Diljit Dosanjh", "image": "https://c.saavncdn.com/artists/Diljit_Dosanjh_003_20230301145647_500x500.jpg", "language": "punjabi"},
+    {"id": "p6", "title": "Naina", "artist": "Diljit Dosanjh", "image": "https://c.saavncdn.com/artists/Diljit_Dosanjh_003_20230301145647_500x500.jpg", "language": "punjabi"},
+    {"id": "p7", "title": "Hass Hass", "artist": "Diljit Dosanjh", "image": "https://c.saavncdn.com/artists/Diljit_Dosanjh_003_20230301145647_500x500.jpg", "language": "punjabi"},
+    {"id": "p8", "title": "Koka", "artist": "Diljit Dosanjh", "image": "https://c.saavncdn.com/artists/Diljit_Dosanjh_003_20230301145647_500x500.jpg", "language": "punjabi"},
+    {"id": "p9", "title": "Clash", "artist": "Diljit Dosanjh", "image": "https://c.saavncdn.com/artists/Diljit_Dosanjh_003_20230301145647_500x500.jpg", "language": "punjabi"},
+    {"id": "p10", "title": "G.O.A.T.", "artist": "Diljit Dosanjh", "image": "https://c.saavncdn.com/artists/Diljit_Dosanjh_003_20230301145647_500x500.jpg", "language": "punjabi"},
+]
+
+PUNJABI_ARTISTS = [
+    {"id": "diljit_dosanjh", "name": "Diljit Dosanjh", "image": "https://c.saavncdn.com/artists/Diljit_Dosanjh_003_20230301145647_500x500.jpg", "language": "punjabi"},
+    {"id": "sidhu_moosewala", "name": "Sidhu Moose Wala", "image": "https://c.saavncdn.com/artists/Sidhu_Moose_Wala_003_20230301145647_500x500.jpg", "language": "punjabi"},
+    {"id": "karan_aujla", "name": "Karan Aujla", "image": "https://c.saavncdn.com/artists/Karan_Aujla_003_20230301145647_500x500.jpg", "language": "punjabi"},
+    {"id": "ap_dhillon", "name": "AP Dhillon", "image": "https://c.saavncdn.com/artists/AP_Dhillon_003_20230301145647_500x500.jpg", "language": "punjabi"},
+    {"id": "shubh", "name": "Shubh", "image": "https://c.saavncdn.com/artists/Shubh_003_20230301145647_500x500.jpg", "language": "punjabi"},
+]
