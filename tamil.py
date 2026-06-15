5029f6fc31106a8dce57ef2512df00e46a47d7c7
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-06-15 22:33:53 +0530

    Create Tamil songs and artists database
    Added a local database of Tamil songs and artists.

--- tamil.py
+++ tamil.py
@@ -1,1 +1,22 @@
+# tamil.py - Tamil Songs Database (Local, no API call needed)
+TAMIL_SONGS = [
+    {"id": "1", "title": "Hukum", "artist": "Anirudh Ravichander", "image": "https://c.saavncdn.com/artists/Anirudh_Ravichander_20191129090352_500x500.jpg", "language": "tamil"},
+    {"id": "2", "title": "Naa Ready", "artist": "Anirudh Ravichander", "image": "https://c.saavncdn.com/artists/Anirudh_Ravichander_20191129090352_500x500.jpg", "language": "tamil"},
+    {"id": "3", "title": "Arabic Kuthu", "artist": "Anirudh Ravichander", "image": "https://c.saavncdn.com/artists/Anirudh_Ravichander_20191129090352_500x500.jpg", "language": "tamil"},
+    {"id": "4", "title": "Kaavaalaa", "artist": "Anirudh Ravichander", "image": "https://c.saavncdn.com/artists/Anirudh_Ravichander_20191129090352_500x500.jpg", "language": "tamil"},
+    {"id": "5", "title": "Ranjithame", "artist": "Thaman S", "image": "https://c.saavncdn.com/artists/Thaman_S_004_20230301111628_500x500.jpg", "language": "tamil"},
+    {"id": "6", "title": "Tum Tum", "artist": "Thaman S", "image": "https://c.saavncdn.com/artists/Thaman_S_004_20230301111628_500x500.jpg", "language": "tamil"},
+    {"id": "7", "title": "Chilla Chilla", "artist": "Anirudh Ravichander", "image": "https://c.saavncdn.com/artists/Anirudh_Ravichander_20191129090352_500x500.jpg", "language": "tamil"},
+    {"id": "8", "title": "Hayyoda", "artist": "Anirudh Ravichander", "image": "https://c.saavncdn.com/artists/Anirudh_Ravichander_20191129090352_500x500.jpg", "language": "tamil"},
+    {"id": "9", "title": "Marakkuma Nenjam", "artist": "A. R. Rahman", "image": "https://c.saavncdn.com/artists/A_R_Rahman_004_20230301101826_500x500.jpg", "language": "tamil"},
+    {"id": "10", "title": "Badass", "artist": "Anirudh Ravichander", "image": "https://c.saavncdn.com/artists/Anirudh_Ravichander_20191129090352_500x500.jpg", "language": "tamil"},
+]
+
+TAMIL_ARTISTS = [
+    {"id": "anirudh_ravichander", "name": "Anirudh Ravichander", "image": "https://c.saavncdn.com/artists/Anirudh_Ravichander_20191129090352_500x500.jpg", "language": "tamil"},
+    {"id": "a_r_rahman", "name": "A. R. Rahman", "image": "https://c.saavncdn.com/artists/A_R_Rahman_004_20230301101826_500x500.jpg", "language": "tamil"},
+    {"id": "sid_sriram", "name": "Sid Sriram", "image": "https://c.saavncdn.com/artists/Sid_Sriram_004_20230301111003_500x500.jpg", "language": "tamil"},
+    {"id": "yuvan_shankar_raja", "name": "Yuvan Shankar Raja", "image": "https://c.saavncdn.com/artists/Yuvan_Shankar_Raja_003_20230301113045_500x500.jpg", "language": "tamil"},
+    {"id": "harris_jayaraj", "name": "Harris Jayaraj", "image": "https://c.saavncdn.com/artists/Harris_Jayaraj_003_20230301112443_500x500.jpg", "language": "tamil"},
+]
 
