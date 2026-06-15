e379a8968733e6137f3e184cfee4b655ce62762d
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-06-15 21:45:50 +0530

    Add files via upload

---
+++ english.py
@@ -0,0 +1,21 @@
+# english.py - English Songs Database (Local, no API call needed)
+ENGLISH_SONGS = [
+    {"id": "e1", "title": "Shape of You", "artist": "Ed Sheeran", "image": "https://c.saavncdn.com/artists/Ed_Sheeran_003_20230301145647_500x500.jpg", "language": "english"},
+    {"id": "e2", "title": "Blinding Lights", "artist": "The Weeknd", "image": "https://c.saavncdn.com/artists/The_Weeknd_002_20230301145647_500x500.jpg", "language": "english"},
+    {"id": "e3", "title": "Levitating", "artist": "Dua Lipa", "image": "https://c.saavncdn.com/artists/Dua_Lipa_003_20230301145647_500x500.jpg", "language": "english"},
+    {"id": "e4", "title": "Stay", "artist": "Justin Bieber", "image": "https://c.saavncdn.com/artists/Justin_Bieber_003_20230301145647_500x500.jpg", "language": "english"},
+    {"id": "e5", "title": "Peaches", "artist": "Justin Bieber", "image": "https://c.saavncdn.com/artists/Justin_Bieber_003_20230301145647_500x500.jpg", "language": "english"},
+    {"id": "e6", "title": "Bad Habits", "artist": "Ed Sheeran", "image": "https://c.saavncdn.com/artists/Ed_Sheeran_003_20230301145647_500x500.jpg", "language": "english"},
+    {"id": "e7", "title": "Save Your Tears", "artist": "The Weeknd", "image": "https://c.saavncdn.com/artists/The_Weeknd_002_20230301145647_500x500.jpg", "language": "english"},
+    {"id": "e8", "title": "Cold Heart", "artist": "Elton John", "image": "https://c.saavncdn.com/artists/Elton_John_003_20230301145647_500x500.jpg", "language": "english"},
+    {"id": "e9", "title": "Industry Baby", "artist": "Lil Nas X", "image": "https://c.saavncdn.com/artists/Lil_Nas_X_003_20230301145647_500x500.jpg", "language": "english"},
+    {"id": "e10", "title": "Good 4 U", "artist": "Olivia Rodrigo", "image": "https://c.saavncdn.com/artists/Olivia_Rodrigo_003_20230301145647_500x500.jpg", "language": "english"},
+]
+
+ENGLISH_ARTISTS = [
+    {"id": "ed_sheeran", "name": "Ed Sheeran", "image": "https://c.saavncdn.com/artists/Ed_Sheeran_003_20230301145647_500x500.jpg", "language": "english"},
+    {"id": "the_weeknd", "name": "The Weeknd", "image": "https://c.saavncdn.com/artists/The_Weeknd_002_20230301145647_500x500.jpg", "language": "english"},
+    {"id": "dua_lipa", "name": "Dua Lipa", "image": "https://c.saavncdn.com/artists/Dua_Lipa_003_20230301145647_500x500.jpg", "language": "english"},
+    {"id": "justin_bieber", "name": "Justin Bieber", "image": "https://c.saavncdn.com/artists/Justin_Bieber_003_20230301145647_500x500.jpg", "language": "english"},
+    {"id": "taylor_swift", "name": "Taylor Swift", "image": "https://c.saavncdn.com/artists/Taylor_Swift_003_20230301145647_500x500.jpg", "language": "english"},
+]
