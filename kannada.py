ea5eb765f15ca133fdfa04a23ad2d363979c9006
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-06-15 22:29:53 +0530

    Create kannada.py

---
+++ kannada.py
@@ -0,0 +1,22 @@
+# kannada.py - Kannada Songs Database (Local, no API call needed)
+KANNADA_SONGS = [
+    {"id": "1", "title": "Salaam Rocky Bhai", "artist": "Ravi Basrur", "image": "https://c.saavncdn.com/artists/Ravi_Basrur_004_20230301121111_500x500.jpg", "language": "kannada"},
+    {"id": "2", "title": "Singara Siriye", "artist": "B. Ajaneesh Loknath", "image": "https://c.saavncdn.com/artists/B_Ajaneesh_Loknath_003_20230301121245_500x500.jpg", "language": "kannada"},
+    {"id": "3", "title": "Toofan", "artist": "Ravi Basrur", "image": "https://c.saavncdn.com/artists/Ravi_Basrur_004_20230301121111_500x500.jpg", "language": "kannada"},
+    {"id": "4", "title": "Ra Ra Rakkamma", "artist": "B. Ajaneesh Loknath", "image": "https://c.saavncdn.com/artists/B_Ajaneesh_Loknath_003_20230301121245_500x500.jpg", "language": "kannada"},
+    {"id": "5", "title": "Mehabooba", "artist": "Ravi Basrur", "image": "https://c.saavncdn.com/artists/Ravi_Basrur_004_20230301121111_500x500.jpg", "language": "kannada"},
+    {"id": "6", "title": "Belageddu", "artist": "Vijay Prakash", "image": "https://c.saavncdn.com/artists/Vijay_Prakash_005_20230301111718_500x500.jpg", "language": "kannada"},
+    {"id": "7", "title": "Yenammi Yenammi", "artist": "Vijay Prakash", "image": "https://c.saavncdn.com/artists/Vijay_Prakash_005_20230301111718_500x500.jpg", "language": "kannada"},
+    {"id": "8", "title": "Sanajna Sanajna", "artist": "Armaan Malik", "image": "https://c.saavncdn.com/artists/Armaan_Malik_003_20230301103859_500x500.jpg", "language": "kannada"},
+    {"id": "9", "title": "Karakusha", "artist": "Arjun Janya", "image": "https://c.saavncdn.com/artists/Arjun_Janya_004_20230301114227_500x500.jpg", "language": "kannada"},
+    {"id": "10", "title": "Tagaru Banthu Tagaru", "artist": "Anthony Daasan", "image": "https://c.saavncdn.com/artists/Anthony_Daasan_003_20230301111516_500x500.jpg", "language": "kannada"},
+]
+
+KANNADA_ARTISTS = [
+    {"id": "ravi_basrur", "name": "Ravi Basrur", "image": "https://c.saavncdn.com/artists/Ravi_Basrur_004_20230301121111_500x500.jpg", "language": "kannada"},
+    {"id": "arjun_janya", "name": "Arjun Janya", "image": "https://c.saavncdn.com/artists/Arjun_Janya_004_20230301114227_500x500.jpg", "language": "kannada"},
+    {"id": "vijay_prakash", "name": "Vijay Prakash", "image": "https://c.saavncdn.com/artists/Vijay_Prakash_005_20230301111718_500x500.jpg", "language": "kannada"},
+    {"id": "b_ajaneesh_loknath", "name": "B. Ajaneesh Loknath", "image": "https://c.saavncdn.com/artists/B_Ajaneesh_Loknath_003_20230301121245_500x500.jpg", "language": "kannada"},
+    {"id": "sanjith_hegde", "name": "Sanjith Hegde", "image": "https://c.saavncdn.com/artists/Sanjith_Hegde_003_20230301120935_500x500.jpg", "language": "kannada"},
+]
+
