9a2af3fabbdc8c57c0d525881a98d79f461386ee
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-06-16 08:09:34 +0530

    Update Hindi songs and artists database with new images

--- hindi.py
+++ hindi.py
@@ -1,21 +1,24 @@
 # hindi.py - Hindi Songs Database (Local, no API call needed)
+
 HINDI_SONGS = [
-    {"id": "1", "title": "Tum Hi Ho", "artist": "Arijit Singh", "image": "https://c.saavncdn.com/artists/Arijit_Singh_002_20230301073043_500x500.jpg", "language": "hindi"},
-    {"id": "2", "title": "Channa Mereya", "artist": "Arijit Singh", "image": "https://c.saavncdn.com/artists/Arijit_Singh_002_20230301073043_500x500.jpg", "language": "hindi"},
-    {"id": "3", "title": "Raataan Lambiyan", "artist": "Jubin Nautiyal", "image": "https://c.saavncdn.com/artists/Jubin_Nautiyal_002_20230301092520_500x500.jpg", "language": "hindi"},
-    {"id": "4", "title": "Kesariya", "artist": "Arijit Singh", "image": "https://c.saavncdn.com/artists/Arijit_Singh_002_20230301073043_500x500.jpg", "language": "hindi"},
-    {"id": "5", "title": "Apna Bana Le", "artist": "Arijit Singh", "image": "https://c.saavncdn.com/artists/Arijit_Singh_002_20230301073043_500x500.jpg", "language": "hindi"},
-    {"id": "6", "title": "Phir Aur Kya Chahiye", "artist": "Arijit Singh", "image": "https://c.saavncdn.com/artists/Arijit_Singh_002_20230301073043_500x500.jpg", "language": "hindi"},
-    {"id": "7", "title": "O Bedardeya", "artist": "Arijit Singh", "image": "https://c.saavncdn.com/artists/Arijit_Singh_002_20230301073043_500x500.jpg", "language": "hindi"},
-    {"id": "8", "title": "Tere Pyaar Mein", "artist": "Arijit Singh", "image": "https://c.saavncdn.com/artists/Arijit_Singh_002_20230301073043_500x500.jpg", "language": "hindi"},
-    {"id": "9", "title": "Satranga", "artist": "Arijit Singh", "image": "https://c.saavncdn.com/artists/Arijit_Singh_002_20230301073043_500x500.jpg", "language": "hindi"},
-    {"id": "10", "title": "Lutt Putt Gaya", "artist": "Arijit Singh", "image": "https://c.saavncdn.com/artists/Arijit_Singh_002_20230301073043_500x500.jpg", "language": "hindi"},
+    {"id": "1",  "title": "Tum Hi Ho",            "artist": "Arijit Singh",  "image": "https://upload.wikimedia.org/wikipedia/en/9/9f/Aashiqui2.jpg",       "language": "hindi"},
+    {"id": "2",  "title": "Channa Mereya",         "artist": "Arijit Singh",  "image": "https://upload.wikimedia.org/wikipedia/en/b/b3/Ae_Dil_Hai_Mushkil.jpg", "language": "hindi"},
+    {"id": "3",  "title": "Raataan Lambiyan",      "artist": "Jubin Nautiyal","image": "https://upload.wikimedia.org/wikipedia/en/8/8b/Shershaah_film_poster.jpg","language": "hindi"},
+    {"id": "4",  "title": "Kesariya",              "artist": "Arijit Singh",  "image": "https://upload.wikimedia.org/wikipedia/en/8/8d/Brahmastra_poster.jpg",  "language": "hindi"},
+    {"id": "5",  "title": "Apna Bana Le",          "artist": "Arijit Singh",  "image": "https://upload.wikimedia.org/wikipedia/en/1/1b/Bhediya_poster.jpg",    "language": "hindi"},
+    {"id": "6",  "title": "Phir Aur Kya Chahiye",  "artist": "Arijit Singh",  "image": "https://upload.wikimedia.org/wikipedia/en/6/60/Zara_Hatke_Zara_Bachke_poster.jpg", "language": "hindi"},
+    {"id": "7",  "title": "O Bedardeya",           "artist": "Arijit Singh",  "image": "https://upload.wikimedia.org/wikipedia/en/2/27/Tu_Jhoothi_Main_Makkaar_poster.jpg", "language": "hindi"},
+    {"id": "8",  "title": "Tere Pyaar Mein",       "artist": "Arijit Singh",  "image": "https://upload.wikimedia.org/wikipedia/en/5/50/Tu_Jhoothi_Main_Makkaar_poster.jpg", "language": "hindi"},
+    {"id": "9",  "title": "Satranga",              "artist": "Arijit Singh",  "image": "https://upload.wikimedia.org/wikipedia/en/d/d5/Animal_2023_film_poster.jpg", "language": "hindi"},
+    {"id": "10", "title": "Lutt Putt Gaya",        "artist": "Arijit Singh",  "image": "https://upload.wikimedia.org/wikipedia/en/7/7f/Dunki_film_poster.jpg", "language": "hindi"},
+    {"id": "11", "title": "Tere Hawaale",          "artist": "Arijit Singh",  "image": "https://upload.wikimedia.org/wikipedia/en/6/6b/Laal_Singh_Chaddha_poster.jpg", "language": "hindi"},
+    {"id": "12", "title": "Ranjha",                "artist": "Jubin Nautiyal","image": "https://upload.wikimedia.org/wikipedia/en/4/4b/Shershaah_film_poster.jpg","language": "hindi"},
 ]
 
 HINDI_ARTISTS = [
-    {"id": "arijit_singh", "name": "Arijit Singh", "image": "https://c.saavncdn.com/artists/Arijit_Singh_002_20230301073043_500x500.jpg", "language": "hindi"},
-    {"id": "jubin_nautiyal", "name": "Jubin Nautiyal", "image": "https://c.saavncdn.com/artists/Jubin_Nautiyal_002_20230301092520_500x500.jpg", "language": "hindi"},
-    {"id": "neha_kakkar", "name": "Neha Kakkar", "image": "https://c.saavncdn.com/artists/Neha_Kakkar_006_20230302130220_500x500.jpg", "language": "hindi"},
-    {"id": "atif_aslam", "name": "Atif Aslam", "image": "https://c.saavncdn.com/artists/Atif_Aslam_003_20230301145647_500x500.jpg", "language": "hindi"},
-    {"id": "shreya_ghoshal", "name": "Shreya Ghoshal", "image": "https://c.saavncdn.com/artists/Shreya_Ghoshal_007_20230301102547_500x500.jpg", "language": "hindi"},
+    {"id": "arijit_singh",   "name": "Arijit Singh",   "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Arijit_Singh_at_the_IIFA_Awards_%282019%29.jpg/440px-Arijit_Singh_at_the_IIFA_Awards_%282019%29.jpg", "genre": "Bollywood"},
+    {"id": "jubin_nautiyal", "name": "Jubin Nautiyal", "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Jubin_Nautiyal_at_Umang_2020.jpg/440px-Jubin_Nautiyal_at_Umang_2020.jpg", "genre": "Bollywood"},
+    {"id": "neha_kakkar",    "name": "Neha Kakkar",    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Neha_Kakkar_at_the_IIFA_Awards_2019.jpg/440px-Neha_Kakkar_at_the_IIFA_Awards_2019.jpg", "genre": "Pop"},
+    {"id": "atif_aslam",     "name": "Atif Aslam",     "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Atif_Aslam_2019.jpg/440px-Atif_Aslam_2019.jpg", "genre": "Bollywood"},
+    {"id": "shreya_ghoshal", "name": "Shreya Ghoshal", "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Shreya_Ghoshal_2012.jpg/440px-Shreya_Ghoshal_2012.jpg", "genre": "Classical/Pop"},
 ]
